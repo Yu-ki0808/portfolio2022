f53faec587545b3aa77be0a35db97bc79cbb2cfb
Yuuki1987 <y.tamura2146@gmail.com>
2022-04-15 15:52:50 +0900

    URL修正

--- portfolio2022/config/adminlte.php
+++ portfolio2022/config/adminlte.php
@@ -188,7 +188,7 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'dashboard',
+    'dashboard_url' => 'muscle-quest/dashboard',
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => 'register',
